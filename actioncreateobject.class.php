20bbc5832624f46d2cfaddd1007722f472ab4f3b
Vladimir Kunin <v.b.kunin@gmail.com>
2015-10-14 01:25:16 +0300

    it works)

--- actioncreateobject.class.php
+++ actioncreateobject.class.php
@@ -66,7 +66,7 @@ class ActionCreateFromTemplate extends ActionCreateObject
       }
       else
       {
-        $oLog->Set('message', 'Notification pending');
+        $oLog->Set('message', 'Object creation pending');
       }
       $oLog->Set('userinfo', UserRights::GetUser());
       $oLog->Set('trigger_id', $oTrigger->GetKey());
@@ -111,19 +111,15 @@ class ActionCreateFromTemplate extends ActionCreateObject
 
   protected function _DoExecute($oTrigger, $aContextArgs, &$oLog)
   {
-    $sPreviousUrlMaker = ApplicationContext::SetUrlMakerClass();
+    // $sPreviousUrlMaker = ApplicationContext::SetUrlMakerClass();
     try
     {
-
       // 1. Получить класс создаваемого объекта
       // 2. Получить набор MappedFields
       // 3. Создать объект
       // 4. Поочередно установить mapped_value в поля объекта
       // 5. Сохранить объект
       // 6. Вернуть id объекта в журнал
-
-      $bRes = false; // Пока не создадим объект и не получим его id
-      $this->m_aErrors = array();
 
       $sClass = $this->Get('obj_class');
       $sOQL = "SELECT MappedField WHERE action_id = ".$this->GetKey();
@@ -135,114 +131,38 @@ class ActionCreateFromTemplate extends ActionCreateObject
       while ($oMappedField = $oMappedFieldSet->Fetch())
       {
         $sAttCode = $oMappedField->Get('mapped_attcode');
+        // TODO: Разобраться с мапингом текстовых полей, где есть что-то помимо плейсходеров.
         $sValue = MetaModel::ApplyParams($oMappedField->Get('mapped_value'), $aContextArgs);
         $oNewObject->Set($sAttCode, $sValue);
       }
-
-      $oNewObject->DBInsertNoReload();
-
-      $oObj = $aContextArgs['this->object()']; // Объект!!!, на котором сработал триггер
-
-      // Determine recicipients
-      //
-      // $sTo = $this->FindRecipients('to', $aContextArgs);
-      // $sCC = $this->FindRecipients('cc', $aContextArgs);
-      // $sBCC = $this->FindRecipients('bcc', $aContextArgs);
-
-      // $sFrom = MetaModel::ApplyParams($this->Get('from'), $aContextArgs);
-      // $sReplyTo = MetaModel::ApplyParams($this->Get('reply_to'), $aContextArgs);
-
-      // $sSubject = MetaModel::ApplyParams($this->Get('subject'), $aContextArgs);
-      // $sBody = MetaModel::ApplyParams($this->Get('body'), $aContextArgs);
-
-
+      $aCheckResults = $oNewObject->CheckToWrite();
+      if ($aCheckResults[0])
+      {
+        // TODO: Добавить в тестовый лог поля и значения.
+        if ($this->IsBeingTested()) { return "Object $sClass will be succesfully created."; }
+        $iKey = $oNewObject->DBInsertNoReload();
+        return "Object $sClass::$iKey succesfully created.";
+      }
+      else
+      {
+        // TODO: Запись лога с ошибками в уведомление.
+        if(!is_null($oLog))
+        {
+          $sErrorLog = '';
+          foreach ($aCheckResults[1] as $sError) {
+            $sErrorLog .= $sError."\n";
+          }
+          $oLog->Set('log', $sErrorLog);
+        }
+        throw new Exception("Object validation failed. Check attcodes and values for creating object.");
+      }
     }
     catch(Exception $e)
     {
-      ApplicationContext::SetUrlMakerClass($sPreviousUrlMaker);
-      throw $e;
+      // ApplicationContext::SetUrlMakerClass($sPreviousUrlMaker);
+      throw $e; // Ошибка выбрасывается на верхний уровень и записывается в лог оповещения.
     }
-    ApplicationContext::SetUrlMakerClass($sPreviousUrlMaker);
-
-    return "Done";
-
-    // if (!is_null($oLog))
-    // {
-    //   if (isset($oNewObject))       $oLog->Set('to', $sTo);
-    // }
-
-    // $oEmail = new EMail();
-
-    // if ($this->IsBeingTested())
-    // {
-    //   $oEmail->SetSubject('TEST['.$sSubject.']');
-    //   $sTestBody = $sBody;
-    //   $sTestBody .= "<div style=\"border: dashed;\">\n";
-    //   $sTestBody .= "<h1>Testing email notification ".$this->GetHyperlink()."</h1>\n";
-    //   $sTestBody .= "<p>The email should be sent with the following properties\n";
-    //   $sTestBody .= "<ul>\n";
-    //   $sTestBody .= "<li>TO: $sTo</li>\n";
-    //   $sTestBody .= "<li>CC: $sCC</li>\n";
-    //   $sTestBody .= "<li>BCC: $sBCC</li>\n";
-    //   $sTestBody .= "<li>From: $sFrom</li>\n";
-    //   $sTestBody .= "<li>Reply-To: $sReplyTo</li>\n";
-    //   $sTestBody .= "<li>References: $sReference</li>\n";
-    //   $sTestBody .= "</ul>\n";
-    //   $sTestBody .= "</p>\n";
-    //   $sTestBody .= "</div>\n";
-    //   $oEmail->SetBody($sTestBody);
-    //   $oEmail->SetRecipientTO($this->Get('test_recipient'));
-    //   $oEmail->SetRecipientFrom($this->Get('test_recipient'));
-    //   $oEmail->SetReferences($sReference);
-    //   $oEmail->SetMessageId($sMessageId);
-    // }
-    // else
-    // {
-    //   $oEmail->SetSubject($sSubject);
-    //   $oEmail->SetBody($sBody);
-    //   $oEmail->SetRecipientTO($sTo);
-    //   $oEmail->SetRecipientCC($sCC);
-    //   $oEmail->SetRecipientBCC($sBCC);
-    //   $oEmail->SetRecipientFrom($sFrom);
-    //   $oEmail->SetRecipientReplyTo($sReplyTo);
-    //   $oEmail->SetReferences($sReference);
-    //   $oEmail->SetMessageId($sMessageId);
-    // }
-
-    // if (empty($this->m_aMailErrors))
-    // {
-    //   if ($this->m_iRecipients == 0)
-    //   {
-    //     return 'No recipient';
-    //   }
-    //   else
-    //   {
-    //     $iRes = $oEmail->Send($aErrors, false, $oLog); // allow asynchronous mode
-    //     switch ($iRes)
-    //     {
-    //       case EMAIL_SEND_OK:
-    //         return "Sent";
-
-    //       case EMAIL_SEND_PENDING:
-    //         return "Pending";
-
-    //       case EMAIL_SEND_ERROR:
-    //         return "Errors: ".implode(', ', $aErrors);
-    //     }
-    //   }
-    // }
-    // else
-    // {
-    //   if (is_array($this->m_aMailErrors) && count($this->m_aMailErrors) > 0)
-    //   {
-    //     $sError = implode(', ', $this->m_aMailErrors);
-    //   }
-    //   else
-    //   {
-    //     $sError = 'Unknown reason';
-    //   }
-    //   return 'Notification was not sent: '.$sError;
-    // }
+    // ApplicationContext::SetUrlMakerClass($sPreviousUrlMaker);
   }
 }
 
@@ -272,6 +192,42 @@ class MappedField extends cmdbAbstractObject
 
     MetaModel::Init_SetZListItems('details', array('action_id', 'mapped_attcode', 'mapped_value'));
     MetaModel::Init_SetZListItems('list', array('mapped_attcode', 'mapped_value'));
+  }
+}
+
+class EventNotificationCreateObject extends EventNotification
+{
+  public static function Init()
+  {
+    $aParams = array
+    (
+      "category" => "core/cmdb,view_in_gui",
+      "key_type" => "autoincrement",
+      "name_attcode" => "",
+      "state_attcode" => "",
+      "reconc_keys" => array(),
+      "db_table" => "priv_event_createobject",
+      "db_key_field" => "id",
+      "db_finalclass_field" => "",
+      "display_template" => "",
+      "order_by_default" => array('date' => false)
+    );
+    MetaModel::Init_Params($aParams);
+    MetaModel::Init_InheritAttributes();
+    MetaModel::Init_AddAttribute(new AttributeText("log", array("allowed_values"=>null, "sql"=>"log", "default_value"=>null, "is_null_allowed"=>true, "depends_on"=>array())));
+    // MetaModel::Init_AddAttribute(new AttributeText("cc", array("allowed_values"=>null, "sql"=>"cc", "default_value"=>null, "is_null_allowed"=>true, "depends_on"=>array())));
+    // MetaModel::Init_AddAttribute(new AttributeText("bcc", array("allowed_values"=>null, "sql"=>"bcc", "default_value"=>null, "is_null_allowed"=>true, "depends_on"=>array())));
+    // MetaModel::Init_AddAttribute(new AttributeText("from", array("allowed_values"=>null, "sql"=>"from", "default_value"=>null, "is_null_allowed"=>true, "depends_on"=>array())));
+    // MetaModel::Init_AddAttribute(new AttributeText("subject", array("allowed_values"=>null, "sql"=>"subject", "default_value"=>null, "is_null_allowed"=>true, "depends_on"=>array())));
+    // MetaModel::Init_AddAttribute(new AttributeHTML("body", array("allowed_values"=>null, "sql"=>"body", "default_value"=>null, "is_null_allowed"=>true, "depends_on"=>array())));
+    // MetaModel::Init_AddAttribute(new AttributeTable("attachments", array("allowed_values"=>null, "sql"=>"attachments", "default_value"=>null, "is_null_allowed"=>true, "depends_on"=>array())));
+
+    MetaModel::Init_SetZListItems('details', array('date', 'message', 'userinfo', 'trigger_id', 'action_id', 'object_id', 'log'));
+    MetaModel::Init_SetZListItems('list', array('date', 'message'));
+
+    // Search criteria
+    // MetaModel::Init_SetZListItems('standard_search', array('name')); // Criteria of the std search form
+    // MetaModel::Init_SetZListItems('advanced_search', array('name')); // Criteria of the advanced search form
   }
 }
 
@@ -399,42 +355,5 @@ class MappedField extends cmdbAbstractObject
   //     return true;
   //   }
 // }
-
-class EventNotificationCreateObject extends EventNotification
-{
-  public static function Init()
-  {
-    $aParams = array
-    (
-      "category" => "core/cmdb,view_in_gui",
-      "key_type" => "autoincrement",
-      "name_attcode" => "",
-      "state_attcode" => "",
-      "reconc_keys" => array(),
-      "db_table" => "priv_event_createobject",
-      "db_key_field" => "id",
-      "db_finalclass_field" => "",
-      "display_template" => "",
-      "order_by_default" => array('date' => false)
-    );
-    MetaModel::Init_Params($aParams);
-    MetaModel::Init_InheritAttributes();
-    // MetaModel::Init_AddAttribute(new AttributeText("to", array("allowed_values"=>null, "sql"=>"to", "default_value"=>null, "is_null_allowed"=>true, "depends_on"=>array())));
-    // MetaModel::Init_AddAttribute(new AttributeText("cc", array("allowed_values"=>null, "sql"=>"cc", "default_value"=>null, "is_null_allowed"=>true, "depends_on"=>array())));
-    // MetaModel::Init_AddAttribute(new AttributeText("bcc", array("allowed_values"=>null, "sql"=>"bcc", "default_value"=>null, "is_null_allowed"=>true, "depends_on"=>array())));
-    // MetaModel::Init_AddAttribute(new AttributeText("from", array("allowed_values"=>null, "sql"=>"from", "default_value"=>null, "is_null_allowed"=>true, "depends_on"=>array())));
-    // MetaModel::Init_AddAttribute(new AttributeText("subject", array("allowed_values"=>null, "sql"=>"subject", "default_value"=>null, "is_null_allowed"=>true, "depends_on"=>array())));
-    // MetaModel::Init_AddAttribute(new AttributeHTML("body", array("allowed_values"=>null, "sql"=>"body", "default_value"=>null, "is_null_allowed"=>true, "depends_on"=>array())));
-    // MetaModel::Init_AddAttribute(new AttributeTable("attachments", array("allowed_values"=>null, "sql"=>"attachments", "default_value"=>null, "is_null_allowed"=>true, "depends_on"=>array())));
-
-    // Display lists
-    MetaModel::Init_SetZListItems('details', array('date', 'message', 'userinfo', 'trigger_id', 'action_id', 'object_id')); // Attributes to be displayed for the complete details
-    MetaModel::Init_SetZListItems('list', array('date', 'message')); // Attributes to be displayed for a list
-
-    // Search criteria
-    //    MetaModel::Init_SetZListItems('standard_search', array('name')); // Criteria of the std search form
-    //    MetaModel::Init_SetZListItems('advanced_search', array('name')); // Criteria of the advanced search form
-  }
-}
 
 ?>
