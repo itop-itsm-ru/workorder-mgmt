036213df3a06a4d0ce0dc55f614bfbae0e0e4cb9
Vladimir Kunin <v.b.kunin@gmail.com>
2015-10-15 01:31:51 +0300

    extends Ticket

--- main.workorder-mgmt.php
+++ main.workorder-mgmt.php
@@ -2,12 +2,12 @@
 
 require_once 'vendor/autoload.php';
 
-// TODO: iBackgroundProcess переделать в iScheduledProcess
+// TODO: iBackgroundProcess переделать в iScheduledProcess?
 class CheckScheduledActivity implements iBackgroundProcess
 {
   public function GetPeriodicity()
   {
-    return 10; // seconds
+    return 60; // seconds
   }
 
   public function Process($iTimeLimit)
@@ -18,36 +18,67 @@ class CheckScheduledActivity implements iBackgroundProcess
     $sOQL = "SELECT ScheduledActivity WHERE status = 'active' AND (action_date < '$sNow' OR (pre_action_enabled = '1' AND pre_action_date < '$sNow'))";
     $oFilter = DBObjectSearch::FromOQL($sOQL);
     $oSet = new DBObjectSet($oFilter);
-    while ((time() < $iTimeLimit) && ($oObj = $oSet->Fetch())) {
-      foreach ($aActionAttCodes as $sActionType => $sAttCode) {
-        if ($oObj->Get($sAttCode) < $sNow) {
+    while ((time() < $iTimeLimit) && ($oObj = $oSet->Fetch()))
+    {
+      foreach ($aActionAttCodes as $sActionType => $sAttCode)
+      {
+        if ($oObj->Get($sAttCode) < $sNow)
+        {
           $aList[] = $oObj->Get('name').': ['.$sAttCode.']';
           // TODO: Делать отдельный триггер на каждую активность?
           $oTriggerSet = new DBObjectSet(
             DBObjectSearch::FromOQL("SELECT TriggerOnScheduledActivity WHERE target_class = 'ScheduledActivity' AND action_type = :action_type"),
             array(), // order by
-            array('activity_id' => $oObj->GetKey(), 'action_type' => $sActionType)
+            array('action_type' => $sActionType)
           );
-          while ($oTrigger = $oTriggerSet->Fetch())
+          if ($oTriggerSet->Count() > 0)
           {
-            // TODO: Дополнительные аргументы для создания нарядов по шаблону
-            $oTrigger->DoActivate($oObj->ToArgs('this'));
+            // wo_start_date - время начала для наряда на работу
+            // wo_end_date - время окончания наряда (если wo_duration == 0, wo_end_date = wo_start_date)
+            //
+            // action_date - время основного действия, установленного в активности (м.б. текущая дата)
+            // next_action_date - время следующего основного действия в контексте текущего основного
+            // main_action_date - время основгого дейтсвия  контексте предварительного
+            $sFormat = AttributeDateTime::GetDateFormat();
+            $aContextArgs = array();
+            if ($sActionType == 'main')
+            {
+              $aContextArgs['this->next_action_date'] = $this->GetActionDate(); // Текущее время не учитывается
+              $aContextArgs['this->wo_start_date'] = $this->Get($sAttCode);
+            }
+            elseif ($sActionType == 'preliminary')
+            {
+              $iActionDate = AttributeDateTime::GetAsUnixSeconds($this->Get($sAttCode)) + $this->GetPreActionInterval();
+              $aContextArgs['this->main_action_date'] = date($sFormat, $iActionDate);
+              $aContextArgs['this->wo_start_date'] = $aContextArgs['this->main_action_date'];
+            }
+            $iWOEndDate = AttributeDateTime::GetAsUnixSeconds($aContextArgs['this->wo_start_date']) + $this->Get('wo_duration');
+            $aContextArgs['this->wo_end_date'] = date($sFormat, $iWOEndDate);
+
+            while ($oTrigger = $oTriggerSet->Fetch())
+            {
+              $oTrigger->DoActivate(array_merge($oObj->ToArgs('this'), $aContextArgs));
+            }
           }
-          // TODO: Если время текущего основного действия и следующего предварительного
-          // совпадают, то в контекст передаются обе настоящие даты. Предварительная дата
-          // должна за собой тянуть соответствующую основную, о которой напоминаем.
-          $oObj->Reset($sAttCode);
+          // TODO: Заменить Reset на обновлнение соответствующей даты.
+          // $oObj->Reset($sAttCode);
         }
       }
-      if($oObj->IsModified())
-      {
-        CMDBObject::SetTrackInfo("Automatic - Scheduled Activity");
-        $oMyChange = CMDBObject::GetCurrentChange();
-        $oObj->DBUpdateTracked($oMyChange, true /*skip security*/);
-      }
+      // Обновление должно выполниться, чтобы пересчитались даты
+      // if($oObj->IsModified())
+      // {
+      CMDBObject::SetTrackInfo("Automatic - Scheduled Activity");
+      $oMyChange = CMDBObject::GetCurrentChange();
+      $oObj->DBUpdateTracked($oMyChange, true /*skip security*/);
+      // }
     }
     $iProcessed = count($aList);
     return "processed scheduled activity: $iProcessed, " .implode(", ", $aList);
+  }
+
+  protected function GetContextArgs($oObj)
+  {
+    # code...
   }
 }
 
